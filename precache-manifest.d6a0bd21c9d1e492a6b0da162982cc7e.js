901b0715fed40e82a0b69b11052290eb16a49610
Marek Vaníček <marcovindy@gmail.com>
2020-10-19 14:49:24 +0200

    Updates

---
+++ precache-manifest.d6a0bd21c9d1e492a6b0da162982cc7e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39151115684effe9d14c11e4e5120911",
+    "url": "/First/index.html"
+  },
+  {
+    "revision": "3326fb0d81a3d7ffc9cb",
+    "url": "/First/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b8c7f965549de52925ee",
+    "url": "/First/static/css/main.f91aa9e3.chunk.css"
+  },
+  {
+    "revision": "3326fb0d81a3d7ffc9cb",
+    "url": "/First/static/js/2.27a16758.chunk.js"
+  },
+  {
+    "revision": "39d5ef359ff3e94666ebc98f7855edf0",
+    "url": "/First/static/js/2.27a16758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c7f965549de52925ee",
+    "url": "/First/static/js/main.78f0b211.chunk.js"
+  },
+  {
+    "revision": "676f3ec936850367357e",
+    "url": "/First/static/js/runtime-main.6d7d298a.js"
+  },
+  {
+    "revision": "82fe896c5a0e47d75854395d1d7dbad1",
+    "url": "/First/static/media/121608453_264594088200792_1362238286302152200_n.82fe896c.png"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/First/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "9cb3441a0fc6a5931dc06aa7f9040ecd",
+    "url": "/First/static/media/aperto.9cb3441a.ttf"
+  },
+  {
+    "revision": "5c690fc70976f9c23d1f2e38dbf6741c",
+    "url": "/First/static/media/facebook.5c690fc7.png"
+  },
+  {
+    "revision": "d42c5b258b5820704fdc84cab71cb666",
+    "url": "/First/static/media/instagram.d42c5b25.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/First/static/media/logo.5d5d9eef.svg"
+  }
+]);
